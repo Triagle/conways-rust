80e96e22902719f60efe36656ffcebc3d26b0a73
Triagle <local@developer.com>
2016-08-05 17:26:01 +1200

    Some niceties and read from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::error::Error;
+use std::fs::File;
+use std::env;
+use std::io::Read;
+use std::path::Path;
 use std::time::Duration;
 use std::thread;
 /*
@@ -14,7 +19,7 @@ fn above_alive(grid: &Vec<Vec<bool>>, pos: (usize, usize)) -> bool {
 }
 fn above_right_alive(grid: &Vec<Vec<bool>>, pos: (usize, usize)) -> bool {
     match pos {
-        (x, y) if y == 0 || x + 1 == grid[0].len() => false,
+        (x, y) if y == 0 || x + 1 == grid[y].len() => false,
         (x, y) => grid[y - 1][x + 1]
     }
 }
@@ -33,7 +38,7 @@ fn left_alive(grid: &Vec<Vec<bool>>, pos: (usize, usize)) -> bool {
 }
 fn right_alive(grid: &Vec<Vec<bool>>, pos: (usize, usize)) -> bool {
     match pos {
-        (x, _) if x + 1 == grid[0].len() => false,
+        (x, y) if x + 1 == grid[y].len() => false,
         (x, y) => grid[y][x + 1]
     }
 }
@@ -45,7 +50,7 @@ fn below_alive(grid: &Vec<Vec<bool>>, pos: (usize, usize)) -> bool {
 }
 fn below_right_alive(grid: &Vec<Vec<bool>>, pos: (usize, usize)) -> bool {
     match pos {
-        (x, y) if y + 1 == grid.len() || x + 1 == grid[0].len() => false,
+        (x, y) if y + 1 == grid.len() || x + 1 == grid[y].len() => false,
         (x, y) => grid[y + 1][x + 1]
     }
 }
@@ -60,7 +65,7 @@ fn below_left_alive(grid: &Vec<Vec<bool>>, pos: (usize, usize)) -> bool {
 fn count_neighbours(grid: &Vec<Vec<bool>>, pos: (usize, usize)) -> usize {
     let directional_functions = [above_alive, above_right_alive, above_left_alive, left_alive, right_alive, below_alive, below_right_alive, below_left_alive];
     match pos {
-        (x, y) if x < grid[0].len() && y < grid.len() => {
+        (x, y) if x < grid[y].len() && y < grid.len() => {
             directional_functions.into_iter().filter(|f| f(grid, pos)).count()
         },
         _ => 0,
@@ -89,19 +94,40 @@ fn print_grid(grid: &Vec<Vec<bool>>) {
         println!("\n");
     }
 }
+fn read_grid(path: &Path) -> Vec<Vec<bool>> {
+    let mut file = match File::open(path) {
+        Err(why) => panic!("Error opening {}: {}", path.display(), why.description()),
+        Ok(file) => file,
+    };
+    let mut s = String::new();
+    file.read_to_string(&mut s).unwrap();
+    s.lines().map(|l| l.split_whitespace().map(|p| if p == "x" {true} else {false}).collect::<Vec<bool>>()).collect::<Vec<Vec<bool>>>()
+}
+fn valid_grid(grid: &Vec<Vec<bool>>) -> bool {
+    match grid.len() {
+        0 => false,
+        _ => grid.iter().map(|l| l.len()).all(|l| l == grid[0].len()),
+    }
+}
+fn clear_lines(lines: usize) {
+    for _ in 0..lines {
+        println!("\x1b[K\x1b[1A");
+    }
+}
 fn main() {
-    let mut grid = vec![
-        vec![false, false, false, false, false],
-        vec![false, false, true, false, false],
-        vec![false, false, false, true, false],
-        vec![false, true, true, true, false],
-        vec![false, false, false, false, false]];
-
-    loop {
-        println!("--------------");
-        print_grid(&grid);
-        println!("--------------");
-        thread::sleep(Duration::from_millis(1000));
-        grid = update(&grid);
+    if let Some(path) = env::args().nth(1) {
+        let mut grid = read_grid(Path::new(&path));
+        if valid_grid(&grid) {
+            loop {
+                print_grid(&grid);
+                thread::sleep(Duration::from_millis(200));
+                grid = update(&grid);
+                clear_lines(grid.len());
+            }
+        } else {
+            println!("Please provide a valid grid (must be regular rectangle of equal length for every row)");
+        }
+    } else {
+        println!("Usage conway <file>");
     }
 }
